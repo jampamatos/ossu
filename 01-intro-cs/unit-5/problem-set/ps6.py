008712beadf3c4dfa50e0acedd0ff44739cdbf77
Jampa Matos <jp.coutm@gmail.com>
2022-07-10 21:29:20 -0300

    Done Problems 1 and 2 of Problem Set 5

--- 01-intro-cs/unit-5/problem-set/ps6.py
+++ 01-intro-cs/unit-5/problem-set/ps6.py
@@ -102,7 +102,16 @@ class Message(object):
         Returns: a dictionary mapping a letter (string) to 
                  another letter (string). 
         '''
-        pass #delete this line and replace with your code here
+        letters = list(string.ascii_lowercase + string.ascii_uppercase)
+        shifted = {}
+
+        for l in letters:
+            l_ord = ord(l) + shift
+            if 65 <= l_ord <= 90 or ord(l) >= 97 and 97 <= l_ord <=122:
+                shifted[l] = chr(l_ord)
+            elif l_ord > 90 or l_ord > 122:
+                shifted[l] = chr(l_ord - 26)
+        return shifted
 
     def apply_shift(self, shift):
         '''
@@ -116,14 +125,22 @@ class Message(object):
         Returns: the message text (string) in which every character is shifted
              down the alphabet by the input shift
         '''
-        pass #delete this line and replace with your code here
+        shift_dic = self.build_shift_dict(shift)
+        shifted_msg = []
+        message = self.get_message_text()
+        for l in message:
+            if l in shift_dic:
+                shifted_msg.append(l.replace(l, shift_dic[l]))
+            else:
+                shifted_msg.append(l)
+        return ''.join(shifted_msg)
 
 class PlaintextMessage(Message):
     def __init__(self, text, shift):
         '''
         Initializes a PlaintextMessage object        
         
-        text (string): the message's text
+        text (string): the message's text -- inherit from message
         shift (integer): the shift associated with this message
 
         A PlaintextMessage object inherits from Message and has five attributes:
@@ -136,7 +153,10 @@ class PlaintextMessage(Message):
         Hint: consider using the parent class constructor so less 
         code is repeated
         '''
-        pass #delete this line and replace with your code here
+        Message.__init__(self,text)
+        self.shift = shift
+        self.encrypting_dict = Message.build_shift_dict(self,self.shift)
+        self.message_text_encrypted = Message.apply_shift(self,self.shift)
 
     def get_shift(self):
         '''
@@ -144,7 +164,7 @@ class PlaintextMessage(Message):
         
         Returns: self.shift
         '''
-        pass #delete this line and replace with your code here
+        return self.shift
 
     def get_encrypting_dict(self):
         '''
@@ -152,7 +172,7 @@ class PlaintextMessage(Message):
         
         Returns: a COPY of self.encrypting_dict
         '''
-        pass #delete this line and replace with your code here
+        return dict(self.encrypting_dict)
 
     def get_message_text_encrypted(self):
         '''
@@ -160,7 +180,7 @@ class PlaintextMessage(Message):
         
         Returns: self.message_text_encrypted
         '''
-        pass #delete this line and replace with your code here
+        return self.message_text_encrypted
 
     def change_shift(self, shift):
         '''
@@ -173,7 +193,9 @@ class PlaintextMessage(Message):
 
         Returns: nothing
         '''
-        pass #delete this line and replace with your code here
+        self.shift = shift
+        self.encrypting_dict = self.build_shift_dict(shift)
+        self.message_text_encrypted = self.apply_shift(shift)
 
 
 class CiphertextMessage(Message):
--- 01-intro-cs/unit-5/test.py
+++ 01-intro-cs/unit-5/test.py
@@ -1,7 +1,23 @@
-def puts_arr(arr):
-  for n in arr:
-    yield n
+import string
 
-arr = [1,2,3,4,5,6]
-a = puts_arr(arr)
-print(a.__next__())
+n = 25
+letters = list(string.ascii_lowercase + string.ascii_uppercase)
+str = 'What a String!'
+shifted = {}
+
+for l in letters:
+  l_ord = ord(l) + n
+  if 65 <= l_ord <= 90 or ord(l) >= 97 and 97 <= l_ord <=122:
+    shifted[l] = chr(l_ord)
+  elif l_ord > 90 or l_ord > 122:
+    shifted[l] = chr(l_ord - 26)
+
+shift_msg = []
+for l in str:
+  if l in shifted:
+    shift_msg.append(l.replace(l, shifted[l]))
+  else:
+    shift_msg.append(l)
+
+print(shifted)
+print(shift_msg)
